4ec18f41ec5af3f39445fe75ae728f6d73086e0b
Ammar Alazzawi <aaalazzawi83@gmail.com>
2020-02-27 18:30:59 -0800

    test

--- config/connection.js
+++ config/connection.js
@@ -4,21 +4,22 @@ var mysql = require ('mysql');
 
 var connection;
 
-if (process.env.JAWSDB_URL)
-{
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-else
-{
+// if (process.env.JAWSDB_URL)
+// {
+// 	connection = mysql.createConnection(process.env.JAWSDB_URL);
+// }
+// else
+// {
 	connection = mysql.createConnection(
 	{
 		host: 'kil9uzd3tgem3naa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	',
 		user: 'uafaowg0y4huycqb',
-		password: "f50stducvaij2fmx",port :3306,
-		
+		password: "f50stducvaij2fmx",
+		port :3306,
+
 		database: 'c6kv4tfh1iq84qd4'
 	});
-};
+//};
 
 
 connection.connect(function(err) 
